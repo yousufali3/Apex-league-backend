49adb2f3f05290e9641204b2cb2d53f9b121e688
yousufali310 <yousuf.ali@codelogicx.com>
2025-05-11 16:32:58 +0530

    s

--- src/routes/adminTournamentRoutes.js
+++ src/routes/adminTournamentRoutes.js
@@ -12,7 +12,7 @@ const router = express.Router();
 
 // Only admin can create tournament
 router.post('/create', verifyToken('admin'), createTournament);
-router.get('get-all-tournaments', getAllTournaments);
+router.get('/get-all-tournaments', getAllTournaments);
 router.put('/room-details/:tournamentId', verifyToken('admin'), addRoomDetails);
 router.put('/kills/:tournamentId', verifyToken('admin'), addKills);
 router.post('/finalize-winners/:tournamentId', finalizeWinners);
